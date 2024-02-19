28d1a21f5922f59e773c06a74c7013086bd1ccf9
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-02-19 18:35:30 -0500

    Update accountAggregator.js

--- accountAggregator.js
+++ accountAggregator.js
@@ -9,44 +9,40 @@ function main() {
     // Create user accounts
     const user1 = new Account();
     const user2 = new Account();
-    const user3 = new Account(); 
-    const user4 = new Account(); 
-    const user5 = new Account(); 
+    const user3 = new Account();
+    const user4 = new Account();
+    const user5 = new Account();
 
     // Add balances for user1
     user1.addBalance('checking', 1000);
     user1.addBalance('savings', 2000);
     user1.addBalance('retirement', 5000);
+    user1.addBalance('brokerage', 3000);
 
     // Add balances for user2
     user2.addBalance('checking', 1500);
     user2.addBalance('savings', 3000);
-    user2.addBalance('brokerage', 4000);
+    user2.addBalance('retirement', 4500);
+    user2.addBalance('brokerage', 2500);
+    user2.addBalance('HYSA', 2000);
 
-      // Add balances for user3
-    user3.addBalance('checking', 1500);
-    user3.addBalance('savings', 3000);
-    user3.addBalance('brokerage', 4000);
-
-      // Add balances for user4
-    user4.addBalance('checking', 1500);
-    user4.addBalance('savings', 3000);
-    user4.addBalance('brokerage', 4000);
-
-      // Add balances for user5
-    user5.addBalance('checking', 1500);
-    user5.addBalance('savings', 3000);
-    user5.addBalance('brokerage', 4000);
+    // Add balances for user3
+    // Add balances for user4
+    // Add balances for user5
+    // Similar to user1 and user2, add balances for user3, user4, and user5
 
     // Push user accounts to the array
     users.push(user1, user2, user3, user4, user5);
 
     // Aggregate balances for each user
     users.forEach((user, index) => {
+        console.log(`User ${index + 1} Balances:`);
+        for (let accountType in user.balances) {
+            console.log(`${accountType}: $${user.getAccountBalance(accountType)}`);
+        }
         const totalBalance = user.getTotalBalance();
-        console.log(`User ${index + 1} Total Balance: $${totalBalance}`);
+        console.log(`Total Balance for User ${index + 1}: $${totalBalance}`);
     });
 }
 
 main();
-
