0638b21c6d6c91f365171c61df201dba6d62f4a1
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-02-19 17:58:48 -0500

    Update accountAggregator.js

--- accountAggregator.js
+++ accountAggregator.js
@@ -3,3 +3,17 @@ const Account = require('./account');
 function main() {
     // Instantiate Account object for the user
     const userAccount = new Account();
+
+    // Add balances from various account types
+    userAccount.addBalance('checking', 1000);
+    userAccount.addBalance('savings', 2000);
+    userAccount.addBalance('retirement', 5000);
+    userAccount.addBalance('brokerage', 3000);
+    userAccount.addBalance('HYSA', 1500);
+
+    // Display total balance
+    const totalBalance = userAccount.getTotalBalance();
+    console.log(`Total Balance: $${totalBalance}`);
+}
+
+main();
