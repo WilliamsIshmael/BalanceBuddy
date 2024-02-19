0417aa323ef8fe64e825a749bba72c16467aed8c
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-02-19 17:59:14 -0500

    Update account.js

--- account.js
+++ account.js
@@ -1,1 +1,24 @@
+class Account {
+    constructor() {
+        this.balances = {};
+    }
+
+    addBalance(accountType, amount) {
+        if (accountType in this.balances) {
+            this.balances[accountType] += amount;
+        } else {
+            this.balances[accountType] = amount;
+        }
+    }
+
+    getTotalBalance() {
+        let total = 0;
+        for (let balance of Object.values(this.balances)) {
+            total += balance;
+        }
+        return total;
+    }
+}
+
+module.exports = Account;
 
