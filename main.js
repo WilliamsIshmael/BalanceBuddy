f7788c6d5efd8a4cf4745f53cb221b0c4e01aa72
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-05-01 21:41:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,23 @@
 // Savings and Investments - accountName, accountType, monthlyContributions, totalContributions
 // totalDebtPayments
 // totalSavings 
+
+interface Debt {
+  item: string; 
+  currentBalance: number; 
+  monthlyPaymentAmount: number; 
+  totalPayments: number; 
+} 
+
+interface SavingsAndInvestment {
+  accountName: string; 
+  accountType: string; 
+  monthlyContributions: number; 
+  totalContributions: number; 
+}
+
+interface PersonalFinance { 
+  totalDebtPayments: number; 
+  totalSavings: number;
+}
+
