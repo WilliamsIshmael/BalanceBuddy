1a71fef77d89c283e4abb8faa83ee34890840605
Ishmael Williams <126845732+WilliamsIshmael@users.noreply.github.com>
2024-05-15 20:41:27 -0400

    Update goals.js

--- goals.js
+++ goals.js
@@ -1,3 +1,25 @@
 // Goals for the user based on milestones on their financial journey. 
 
-const { calculateTotalDebtPayments } = require('./main.js');
+const { totalDebtPayments, totalContributions } = require('./main.js');
+
+const debtPaymentGoals = [500, 1000, 10000, 50000, 100000];
+const contributionGoals = [500, 1000, 10000, 50000, 100000];
+
+for (const debtPaymentGoal of debtPaymentGoals) {
+  const hasReachedDebtPaymentGoal = totalDebtPayments >= debtPaymentGoal;
+  if (hasReachedDebtPaymentGoal) {
+    console.log(
+      `Congratulations! You have reached the ${debtPaymentGoal} debt payment goal!`
+);
+  }
+}
+
+for (const contributionGoal of contributionGoals) {
+  const hasReachedContributionGoal = totalContributions >= contributionGoal;
+  if (hasReachedContributionGoal) {
+    console.log(
+      `Congratulations! You have reached the ${contributionGoal} contribution goal!`
+);
+  }
+}
+
